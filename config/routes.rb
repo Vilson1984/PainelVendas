4fa8ddd594698be3249bb2540ae0b4fb2d8570e7
Vilson1984 <vcmorion@hotmail.com>
2022-07-02 19:21:47 -0300

    Login user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  
+  resources :users
+  get    'sign_in'   => 'sessions#new'
+  post   'sign_in'   => 'sessions#create'
+  delete 'sign_out'  => 'sessions#destroy'
+  
+  
+
+  get 'users/new'
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "home#index"
+   #root "home#index"
+    root 'sessions#new'
 end
